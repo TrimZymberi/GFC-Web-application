33010de3ef008c310a897c1909a6fed6af93a71e
TrimZymberi <tz58110@ubt-uni.net>
2023-04-15 20:46:21 +0200

    Menu items
    Adding a menu tabs for different categories with menu products.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React from "react";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp';
+import Home from "./Home";
+import Cart from "./Cart";
 
 function App() {
   return (
     <div className="App">
       
       <NavbarComp/>
-
+      <Home/>
     </div>
   );
 }
