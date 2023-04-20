d080c9ab43d58a235d947a627c57964b88f4180b
“RionSermaxhaj” <rs58539@ubt-uni.net>
2023-04-20 23:20:44 +0200

    Footer

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 export default function AboutUs() {
   return (
     <div>
       <h1>About Us</h1>
+
+      <Footer />
     </div>
+
+    
   )
 }
