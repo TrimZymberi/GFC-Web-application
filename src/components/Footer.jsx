2219fa3d6486290ade9d348ec92c9e589919f5d7
TrimZymberi <tz58110@ubt-uni.net>
2023-04-21 00:18:50 +0200

    TEST

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                     <h4>Contact us</h4>
                     <ul className="list-unstyled">
                         <li>Gjilan 60000, Kosove</li>
-                        <li>Rr.Mulla Idrizi</li> 
+                        <li>Rr. Trim huruglica</li> 
                         <li>+383 44 123 456</li>
                         <li>+383 49 123 456</li>
                        
