09fe87193a0c8b072cae64d6965bee3d1b89707e
armql <am57839@ubt-uni.net>
2023-04-23 19:41:02 +0200

    footer re-added
    Co-Authored-By: RionSermaxhaj <120327239+RionSermaxhaj@users.noreply.github.com>

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbars from '../components/Navbars'
+import Footer from '../components/Footer'
+
 
 export default function DefaultLayout() {
   return (
     <>
       <Navbars />
       <Outlet />
+      <Footer />
     </>
   )
 }
