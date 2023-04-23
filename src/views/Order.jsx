87faaa66d0465649aa551894614e6fa33c06135e
TrimZymberi <tz58110@ubt-uni.net>
2023-04-23 17:23:51 +0200

    Creating the order page
    When clicking the pic in Menu ad in main page to navigate to another page where user can make an order
    [

---
+++ src/views/Order.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/tab-style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function Order() {
+  return (
+    <div>
+      <h1>Order Page</h1>
+    </div>
+
+  )
+}
