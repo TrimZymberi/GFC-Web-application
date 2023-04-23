88f6181132f2f7862681cdbef93c8f726c726b36
TrimZymberi <tz58110@ubt-uni.net>
2023-04-23 23:04:22 +0200

    OrderCategories
    Making an order categories where there are products and customer can put his products in cart and purchase.

--- src/views/Order.jsx
+++ src/views/Order.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../styles/tab-style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import OrderCategories from "../components/OrderCategories"
 
 
 export default function Order() {
   return (
     <div>
       <h1>Order Page</h1>
+      <OrderCategories />
     </div>
 
   )
