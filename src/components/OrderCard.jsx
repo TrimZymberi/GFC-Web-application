da896362a3fbab4db977504bda56cb7e7e5370d2
TrimZymberi <tz58110@ubt-uni.net>
2023-04-23 22:58:10 +0200

    Create OrderCard.jsx

---
+++ src/components/OrderCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export default function ItemCard(props) {
+  const location = useLocation();
+  const links = [{ to: '/order'}];
+
+  return (
+    <div className="card-group" style={{ width: '40rem', height: '20rem', margin: '-1.15rem', }}>
+      <div className="card  text-center" style={{ margin: '1.2rem' }}>
+        <img src={props.img} className="//" style={{ width: '8rem' }} />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <h5 className="card-title text-danger">{props.price} €</h5>
+          <p className="card-text">{props.desc}</p>
+          <button className="btn btn-outline-danger ">Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+}
