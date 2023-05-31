1211b5405723a41464be3eb96993ba1d449cdb29
Syartaa <syartapajaziti@gmail.com>
2023-05-30 22:55:10 -0700

    driverdashboard main and chart

---
+++ GfcRct/src/layouts/Employee/components/DashboardView.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {FaSearch,FaRegBell,FaEnvelope} from 'react-icons/fa'
+
+export default function DashboardView() {
+  return (
+    <div className='flex items-center justify-between bg-[#F9F5F6]  h-[70px] shadow-lg px-[25px]'>
+        <div className='flex items-center rounded-[5px]'>
+            <input type="text" className='bg-[#F8F9FC] h-[40px] border-none outline-none pl-[13px] w-[350px] rounded-[5px] placeholder:text-[14px] leading-[20px] font-normal' placeholder='Search for ...' />
+            <div className='bg-[#CD1818] h-[40px] px-[14px] flex items-center justify-center cursor-pointer rounded-tr-[5px] rounded-br-[5px]'>
+                <FaSearch color="white"/>
+            </div>
+        </div>
+
+        <div className='flex items-center gap-[15px] relative'>
+            <div className='flex items-center gap-[25px] border-r-[3px] pr-[25px]'>
+                <FaRegBell/>
+                <FaEnvelope/>
+            </div>
+            <div className='flex items-center gap-[15px] relative'>
+                <p>Syarta Pajaziti</p>
+            </div>
+        </div>
+    </div>
+  );
+}
--- GfcRct/src/layouts/Employee/views/DriverDashboard.jsx
+++ GfcRct/src/layouts/Employee/views/DriverDashboard.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import driver from '../image/driver.png'
-import SideBar from '../components/SideBar';
-
+import React from "react";
+import driver from "../image/driver.png";
+import SideBar from "../components/SideBar";
+import DashboardView from "../components/DashboardView";
+import DashboardMain from "../components/DashboardMain";
 
 export default function DriverDashboard() {
-  return (
-   <div className='flex'>
-    <div className='basis-[12%] h-[100vh] '>
-        <SideBar/>
-    </div>
-   </div>
-  );
+    return (
+        <div className="flex">
+            <div className="basis-[12%] h-[100vh] ">
+                <SideBar />
+            </div>
+            <div className="basis-[88%] border">
+                <DashboardView />
+
+                <DashboardMain />
+            </div>
+
+            <div></div>
+        </div>
+    );
 }
