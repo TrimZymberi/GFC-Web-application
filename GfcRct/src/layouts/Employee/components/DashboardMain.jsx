f59cafcb4dfd341de5b436ef7f28bf34cab08e6f
“RionSermaxhaj” <rs58539@ubt-uni.net>
2023-06-02 17:49:38 +0200

    Update employee pannel

--- GfcRct/src/layouts/Employee/components/DashboardMain.jsx
+++ GfcRct/src/layouts/Employee/components/DashboardMain.jsx
@@ -10,18 +10,29 @@ export default function DashboardE() {
                 <h1 className="text-[#5a5c69] text-[28px] leading-[34px] font-normal cursor-pointer">
                     Dashboard
                 </h1>
+
+
+                <button className="bg-[#6dc089] h-[35px] rounded-[3px] text-white flex items-center justify-center px-[30px] cursor-pointer">
+                    Accept incoming order
+                </button>
+                
+
                 <button className="bg-[#B70404] h-[35px] rounded-[3px] text-white flex items-center justify-center px-[30px] cursor-pointer">
                     Generate Report
                 </button>
+
+
+
+
             </div>
             <div className="grid grid-cols-4 gab-[50px] mt-[25px] pb-[15px]">
                 <div className="h-[100px] rounded-[8px] bg-white border-l-[4px] border-[#B70404] flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
                     <div>
                         <h2 className="text-[#B70404] text-[11px] leading-[17px] font-bold">
-                            EARNINGS (MONTHLEY)
+                            Orders (MONTHLY)
                         </h2>
                         <h1 className="text-[20px] leading-[24px] font-bold text-[#5a5c69] mt-[5px]">
-                            $40,000
+                            30,000
                         </h1>
                     </div>
                     <FaRegCalendarMinus fontSize={28} color="" />
@@ -40,7 +51,7 @@ export default function DashboardE() {
                 <div className="h-[100px] rounded-[8px] bg-white border-l-[4px] border-[#B70404] flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
                     <div>
                         <h2 className="text-[#B70404] text-[11px] leading-[17px] font-bold">
-                            TASKS
+                             PENDING Orders
                         </h2>
                         <h1 className="text-[20px] leading-[24px] font-bold text-[#5a5c69] mt-[5px]">
                             $40,000
@@ -51,7 +62,7 @@ export default function DashboardE() {
                 <div className="h-[100px] rounded-[8px] bg-white border-l-[4px] border-[#B70404] flex items-center justify-between px-[30px] cursor-pointer hover:shadow-lg transform hover:scale-[103%] transition duration-300 ease-out">
                     <div>
                         <h2 className="text-[#B70404] text-[11px] leading-[17px] font-bold">
-                            PENDING REQUEST
+                            Cancelled Orders
                         </h2>
                         <h1 className="text-[20px] leading-[24px] font-bold text-[#5a5c69] mt-[5px]">
                             $40,000
--- GfcRct/src/layouts/Employee/components/Sidebar.jsx
+++ GfcRct/src/layouts/Employee/components/Sidebar.jsx
@@ -44,7 +44,7 @@ export default function SideBar() {
                     <div className="flex items-center gap-[10px]">
                         <FaRegChartBar color="white" />
                         <p className="text-[14px] leading-[20px] font-normal text-white">
-                            Opcomming orders
+                            Upcomming orders
                         </p>
                     </div>
                     <FaChevronRight color="white" />
