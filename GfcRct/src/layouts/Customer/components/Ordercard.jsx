93ad6a81e48eda52eecf159de35e9df8e7d2cd52
armql <am57839@ubt-uni.net>
2023-06-08 14:44:57 +0200

    minor fix

--- GfcRct/src/layouts/Customer/components/Ordercard.jsx
+++ GfcRct/src/layouts/Customer/components/Ordercard.jsx
@@ -15,7 +15,7 @@ export default function Ordercard(props) {
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
                 </a>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.price}EUR</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.retail_price}EUR</span>
                     <a href="#" 
                     onClick={() =>addItem(props.item)}
                     className="hover:text-white hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Add to Cart <span aria-hidden="true">&rarr;</span></a>
